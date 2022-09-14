64217764965f8a1cda8cbe6e87c6984a894f7ef6
emmaodia <odia.emma@gmail.com>
2022-09-14 23:36:24 +0100

    Advanced Storage Tests improved.

--- day-one/test/AdvancedStorage.js
+++ day-one/test/AdvancedStorage.js
@@ -2,9 +2,13 @@ const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Advanced Storage", () => {
-  it("It should stored data", async () => {
-    const Contract = await ethers.getContractFactory("AdvancedStorage");
-    const contract = await Contract.deploy();
+  before(async () => {
+    let Contract = await ethers.getContractFactory("AdvancedStorage");
+    contract = await Contract.deploy();
+  });
+  it("It should add data to the array", async () => {
+    // const Contract = await ethers.getContractFactory("AdvancedStorage");
+    // const contract = await Contract.deploy();
     // console.log(contract);
     await contract.add(1);
     const result = await contract.get(0);
@@ -12,9 +16,9 @@ describe("Advanced Storage", () => {
     expect(result).equal(1);
   });
 
-  it("It should return stored data", async () => {
-    const Contract = await ethers.getContractFactory("AdvancedStorage");
-    const contract = await Contract.deploy();
+  it("It should return data from the array", async () => {
+    // const Contract = await ethers.getContractFactory("AdvancedStorage");
+    // const contract = await Contract.deploy();
 
     await contract.add(1);
 
@@ -24,8 +28,8 @@ describe("Advanced Storage", () => {
   });
 
   it("It should return all the stored items on the array", async () => {
-    const Contract = await ethers.getContractFactory("AdvancedStorage");
-    const contract = await Contract.deploy();
+    // const Contract = await ethers.getContractFactory("AdvancedStorage");
+    // const contract = await Contract.deploy();
 
     await contract.add(1);
     await contract.add(1);
@@ -36,6 +40,12 @@ describe("Advanced Storage", () => {
     const result = await contract.getAll();
     console.log(result);
     console.log(result.length);
-    expect(result.length).equal(5);
+    expect(result.length).equal(7);
+  });
+
+  it("It should get the ids array", async () => {
+    const result = await contract.getAll();
+    const nums = result.map((num) => num.toNumber());
+    assert.deepEqual(nums, [1, 1, 1, 1, 1, 1, 1]);
   });
 });
