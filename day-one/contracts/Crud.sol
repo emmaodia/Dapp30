9360a7552d7e82815efd9752deaaf4702818e372
emmaodia <odia.emma@gmail.com>
2022-09-15 23:22:12 +0100

    CRUD Contract.

---
+++ day-one/contracts/Crud.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+contract Crud {
+    struct User {
+        uint id;
+        string name;
+    }
+
+    User[] public users;
+    uint public nextId;
+
+    function create(string memory name) public {
+        users.push(User(nextId, name));
+        nextId++;
+    }
+
+    function read(uint _id) public view returns (uint, string memory) {
+        uint i = find(id);
+        return (users[i].id, users[i].name);
+        // for(uint i = 0; i < users.length; i++) {
+        //     if(users[i].id == _id) {
+        //         return(users[i].id, users[i].name);
+        //     }
+        // }
+    }
+
+    function update(uint id, string memory name) public {
+        uint i = find(id);
+        users[i].name = name;
+        // for(uint i = 0; i < users.length; i++) {
+        //     if(users[i].id == _id) {
+        //         users[i].name = name;
+        //     }
+        // }
+    }
+
+    function destroy(uint id) public {
+        delete users[id];
+    }
+
+    function find(uint id) internal view returns (uint) {
+        for (uint i = 0; i < users.length; i++) {
+            if (users[i].id == _id) {
+                return i;
+            }
+        }
+    }
+}
